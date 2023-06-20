851e54b0f49a0b97a7c8d282f29f5323e6898507
ferrreo <harderthanfire@gmail.com>
2023-06-20 17:11:28 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,9 +3,9 @@
 DEBIAN_FRONTEND=noninteractive
 
 # Clone Upstream
-mkdir -p ./src-pkg-name
-cp -rvf ./debian ./src-pkg-name/
-cd ./src-pkg-name/
+git clone https://gitlab.com/ananicy-cpp/ananicy-cpp.git
+cd ananicy-cpp
+cp -rvf ../debian ./
 
 # Get build deps
 apt-get build-dep ./ -y
